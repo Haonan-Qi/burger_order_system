c55f1a283c4c824a4d23e10d00c42b5e82a06739
Haonan.Qi <haonan.steven@gmail.com>
2020-04-25 19:29:38 +1000

    Burger component setup

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, { Component,Fragment } from 'react';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
 
     render () {
         return (
             <Fragment>
-                <p>Burger</p>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Fragment>
         );
