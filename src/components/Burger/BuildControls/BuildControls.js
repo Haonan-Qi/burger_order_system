a30b95e613c6a7df5312b05741f3f38ac28f65b3
Haonan.Qi <haonan.steven@gmail.com>
2020-04-25 22:57:22 +1000

    Connect state to BurgerControl

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import classes from './BuildControls.module.css';
-import BuildControl from './BuildControl/BuildControl';
+import React from "react";
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
 
 const buildControls = (props) => (
-    <div className={classes.BuildControls}>
+  <div className={classes.BuildControls}>
+    {props.controls.map((type) => (
+      <BuildControl label={type}
+      added={() => props.ingredientAdded(type)}
+      removed={() => props.ingredientRemoved(type)} />
+    ))}
 
-        {props.controls.map(type => <BuildControl label={type} />)}
-
-        <button 
-            className={classes.OrderButton}
-            disabled={!props.purchasable}>ORDER NOW</button>
-    </div>
+    <button className={classes.OrderButton} disabled={!props.purchasable}>
+      ORDER NOW
+    </button>
+  </div>
 );
 
 export default buildControls;
