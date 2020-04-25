b56890f37ba5791e837818c579639c67cd8408f3
Haonan.Qi <haonan.steven@gmail.com>
2020-04-25 19:01:49 +1000

    burger builder page container component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
+
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <p>Test Paragraph</p>
+      <BurgerBuilder></BurgerBuilder>
+    </Layout>
+  );
 }
 
 export default App;
