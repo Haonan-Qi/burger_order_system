19db9d72c0d086469dca110c48f9199ced712cba
Haonan.Qi <haonan.steven@gmail.com>
2020-04-25 19:11:08 +1000

    + propTypes validation to BurgerIngredient

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import classes from "./BurgerIngredient.css";
 
 class BurgerIngredient extends Component {
@@ -36,5 +37,9 @@ class BurgerIngredient extends Component {
     return ingredient;
   }
 }
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
+};
 
 export default BurgerIngredient;
