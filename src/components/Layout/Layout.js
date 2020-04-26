997f7b9da293e40e7580210dc971eb75274b250b
Haonan.Qi <haonan.steven@gmail.com>
2020-04-26 18:54:34 +1000

    + SideDrawer (Responsive version)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React,{Fragment} from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Burger/Navigation/ToolBar/Toolbar';
+import SideDrawer from '../Burger/Navigation/SideDrawer/SideDrawer';
 
 
 const layout = ( props ) => (
     <Fragment>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <Toolbar />
-
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
