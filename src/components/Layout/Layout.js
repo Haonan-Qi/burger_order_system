696e46e7de0a79d0db0a454528d2b9c10af10a8d
Haonan.Qi <haonan.steven@gmail.com>
2020-04-26 17:22:32 +1000

    + ToolBar & Logo & Reuseable NavigationItem

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React,{Fragment} from 'react';
 import classes from './Layout.module.css';
+import Toolbar from '../Burger/Navigation/ToolBar/Toolbar';
 
 
 const layout = ( props ) => (
     <Fragment>
         <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
+
         <main className={classes.Content}>
             {props.children}
         </main>
